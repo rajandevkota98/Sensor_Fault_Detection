d5d13514d07d7fe59dfbb4de5c505f58ca76b7a0
rajandevkota98 <r.devkota.98@gmail.com>
2023-07-02 11:49:23 +0545

    setup completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sensor',
+    version='1.0.1',
+    description='this is end to end machine learning project',
+    author='Rajan Devkota',
+    author_email='r.devkota.98@gmail.com',
+    packages=find_packages()
+)
+
