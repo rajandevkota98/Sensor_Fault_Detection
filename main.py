081690c5bc3700d48ac06ad397d41e091b4edd90
rajandevkota98 <r.devkota.98@gmail.com>
2023-07-03 09:08:05 +0545

    logging completed

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from sensor.configuration.mongodb_connection import MongoDBClient
 
+from sensor.exception import SensorException
+from sensor.logger import logging
+import os, sys
+
+def test_exception():
+    try:
+        logging.info('into the try block')
+        x = 5
+        y = 0
+        return  x/y
+    except Exception as e:
+        raise SensorException(e,sys)
+    
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print(mongodb_client.database.list_collection_names())
+    try:
+        test_exception()
+    except Exception as e:
+        raise SensorException(e,sys)
