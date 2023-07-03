5f3769aeb3ae54c0d9dfb4e057ecf673f8323add
rajandevkota98 <r.devkota.98@gmail.com>
2023-07-03 08:35:28 +0545

    exception done

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from sensor.configuration.mongodb_connection import MongoDBClient
 
 if __name__ == '__main__':
     mongodb_client = MongoDBClient()
-
     print(mongodb_client.database.list_collection_names())
