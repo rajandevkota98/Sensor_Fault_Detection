20e4f5ebe141b53184457dd3218b9a1f6e9994a0
rajandevkota98 <r.devkota.98@gmail.com>
2023-07-05 15:24:55 +0545

    data validation pipeline done

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,9 +1,10 @@
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 from sensor.exception import SensorException
 from sensor.logger import logging
 import os,sys
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 class TrainPipeline:
     def __init__(self) -> None:
@@ -20,13 +21,20 @@ class TrainPipeline:
         except Exception as e:
             raise SensorException(e,sys)
         
-    def start_data_validation(self):
+
+        
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
         try:
+
             logging.info('starting data validation')
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
+            data_validation_artifact = data_validation.initiate_data_validation()
             logging.info('ending data validation')
+            return data_validation_artifact
         except Exception as e:
             raise SensorException(e,sys)
-            
+                    
     def start_data_transformation(self):
         try:
             logging.info('starting data transformation')
